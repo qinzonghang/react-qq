de2852857ffec32f05d4f0f8f0841769d752ac7e
QINZONGHANG <694825691@qq.com>
2018-08-08 17:09:28 +0800

    swiper布局

--- qq-music/src/App.jsx
+++ qq-music/src/App.jsx
@@ -14,33 +14,66 @@ import My from './components/my.jsx'
 import Music from './components/music.jsx'
 import Find from './components/find.jsx'
 
-import Header from './components/header.jsx'
 import Footer from './components/footer.jsx'
 
-class App extends Component {
-  render() {
-    return <Router>
-      <div className="router">
-        <Route exact path="/" render={()=>{
-          return <Redirect to="/s"/>
-        }}/>
+import './font/iconfont.css'
 
-        <Route path="/s" render={()=>{
-          return <div className="App">
-             <header><Header/></header>
+import Swiper from 'swiper/dist/js/swiper.min.js';
+import 'swiper/dist/css/swiper.min.css';
+
+class App extends Component {
+  constructor(props){
+        super(props)
+        this.state={
+            options : ["我的","音乐馆","发现"]
+        }
+    }
+  render() {
+    return <div className="App">
+             <header>
+             <div className="header-wrap">
+                <div className="top">
+                    <i className="icon iconfont icon-filter-"></i>
+                    <div className="options">{
+                          this.state.options.map((item,index)=>{
+                                return  <span key={index} onClick={()=>{this.Click(index)}} className="lick">{item}</span>
+                          })     
+                    }</div>
+                    <i className="icon iconadd iconfont icon-add"></i>
+                </div>
+                <div className="input">
+                  <p><i className="icon iconfont icon-sousuo"></i>
+                  <span>搜索</span></p>
+                </div>
+            </div>
+             </header>
              <section>
-               <Route exact path="/s" render={()=>{
-                 return <Redirect to="/s/music"/>
-               }}/>
-               <Route path="/s/my" component={My}/>
-               <Route path="/s/music" component={Music}/>
-               <Route path="/s/find" component={Find}/>
+             <div className="swiper-container">
+                    <div className="swiper-wrapper">
+                        <div className="swiper-slide">我的</div>
+                        <div className="swiper-slide">音乐</div>
+                        <div className="swiper-slide">发现</div>
+                    </div>
+                </div> 
              </section>
              <footer><Footer/></footer>
           </div>
-        }}/>
-      </div>
-    </Router>
+  }
+  componentDidMount(){
+    this.swiper = new Swiper('.swiper-container', {
+     
+      // loop: true,
+      // autoplay: true,
+      // on: {
+      //     slideChangeTransitionEnd: function(){
+      //       console.log(this.activeIndex);//切换结束时，告诉我现在是第几个slide
+      //     },
+      //   },
+  })
+  }
+  Click(index){
+    console.log(index)
+    
   }
 }
 
